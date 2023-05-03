03fc51f15d4bbc594907366cd9672a6373e23bcb
JamesBremner <james@ravenspoint.com>
2023-05-03 10:38:00 -0400

    Manhattan paths

--- src/cGrid2D.h
+++ src/cGrid2D.h
@@ -2,8 +2,9 @@
 class cGrid2D
 {
 public:
-    typedef std::pair<int,int>  point_t;
-    typedef std::pair<point_t,point_t> edge_t;
+    typedef std::pair<int, int> point_t;
+    typedef std::pair<point_t, point_t> edge_t;
+    typedef std::vector<point_t> path_t;
 
     cGrid2D()
     {
@@ -24,15 +25,15 @@ public:
         if (x < 0 || y < 0)
             throw std::runtime_error(
                 "Bad cGrid2D co-ords");
-        if (x > myColCount-1)
-            myColCount = x+1;
-        if (y > myRowCount-1)
-            myRowCount = y+1;
+        if (x > myColCount - 1)
+            myColCount = x + 1;
+        if (y > myRowCount - 1)
+            myRowCount = y + 1;
     }
 
     /// @brief set grid dimensions
-    /// @param colCount 
-    /// @param rowCount 
+    /// @param colCount
+    /// @param rowCount
 
     void setDim(int colCount, int rowCount)
     {
@@ -49,8 +50,8 @@ public:
     }
 
     /// @brief get grid dimensions
-    /// @param[out] colCount 
-    /// @param[out] rowCount 
+    /// @param[out] colCount
+    /// @param[out] rowCount
 
     void getDim(int &colCount, int &rowCount) const
     {
@@ -59,22 +60,22 @@ public:
     }
 
     /// @brief get vertex index from location
-    /// @param col 
-    /// @param row 
+    /// @param col
+    /// @param row
     /// @return vertex index
 
     int index(int col, int row) const
     {
-        if( 0 > row || row > myRowCount-1 ||
-            0 > col || col > myColCount )
+        if (0 > row || row > myRowCount - 1 ||
+            0 > col || col > myColCount)
             throw std::runtime_error(
-                "cGrid2d::index bad col,row"            );
+                "cGrid2d::index bad col,row");
         return row * myColCount + col;
     }
 
     /// @brief get vertex location from index
-    /// @param[out] col 
-    /// @param[out] row 
+    /// @param[out] col
+    /// @param[out] row
     /// @param index
 
     void coords(int &col, int &row, int index) const
@@ -106,20 +107,122 @@ public:
     getEdges() const
     {
         std::vector<edge_t> ret;
-        for( auto& e : vEdge )
+        for (auto &e : vEdge)
         {
-            int x1,y1,x2,y2;
-            coords(x1,y1,e.first);
-            coords(x2,y2,e.second);
+            int x1, y1, x2, y2;
+            coords(x1, y1, e.first);
+            coords(x2, y2, e.second);
             ret.push_back(
                 std::make_pair(
-                    std::make_pair(x1,y1),std::make_pair(x2,y2)));
+                    std::make_pair(x1, y1), std::make_pair(x2, y2)));
         }
         return ret;
     }
 
-    private:
+    /// @brief Manhatten path, horizontal part
+    /// @param s starting point
+    /// @param d destination point
+    /// @return path, including start
+    static path_t
+    X(const point_t &s, const point_t &d)
+    {
+        path_t ret;
+        ret.push_back(s);
+        int deltax = d.first - s.first;
+        if (deltax)
+        {
+            int inc = 1;
+            if (deltax < 0)
+                inc = -1;
+            int x = s.first;
+            do
+            {
+                x += inc;
+                ret.push_back(std::make_pair(x, s.second));
+            } while (x != d.first);
+        }
+        return ret;
+    }
+
+    /// @brief Manhatten path, vertical part
+    static path_t
+    Y(const point_t &s, const point_t &d)
+    {
+        std::vector<std::pair<int, int>> ret;
+        ret.push_back(s);
+        int deltay = d.second - s.second;
+        if (deltay)
+        {
+            int inc = 1;
+            if (deltay < 0)
+                inc = -1;
+            int y = s.second;
+            do
+            {
+                y += inc;
+                ret.push_back(std::make_pair(s.first, y));
+            } while (y != d.second);
+        }
+        return ret;
+    }
+
+    /// @brief Manhatten path, diagonal steps
+    static path_t
+    D(const point_t &s, const point_t &d)
+    {
+        std::vector<std::pair<int, int>> ret;
+        ret.push_back(s);
+        int deltax = d.first - s.first;
+        int deltay = d.second - s.second;
+        int incx = 1;
+        int incy = 1;
+        if (deltax < 0)
+            incx = -1;
+        if (deltay < 0)
+            incy = -1;
+        int x = s.first;
+        int y = s.second;
+        while ((x != d.first) || (y != d.second))
+        {
+            if (x == d.first)
+                incx = 0;
+            if (y == d.second)
+                incy = 0;
+            x += incx;
+            y += incy;
+            ret.push_back(std::make_pair(x, y));
+        }
+        return ret;
+    }
+
+    /// @brief Manhatten path, vertical than horizontal
+    static path_t
+    YX(const point_t &s, const point_t &d)
+    {
+        auto path = Y(s, d);
+        auto path2 = X(point_t(s.first, d.second), d);
+        path.insert(
+            path.end(),
+            path2.begin() + 1,
+            path2.end());
+        return path;
+    }
+
+    /// @brief Manhatten path, horizontal then vertical
+    static path_t
+    XY(const point_t &s, const point_t &d)
+    {
+        auto path = X(s, d);
+        auto path2 = Y(point_t(d.first, s.second), d);
+        path.insert(
+            path.end(),
+            path2.begin() + 1,
+            path2.end());
+        return path;
+    }
+
+private:
     int myColCount;
     int myRowCount;
-    std::vector<std::pair<int,int>> vEdge;
+    std::vector<std::pair<int, int>> vEdge;
 };
